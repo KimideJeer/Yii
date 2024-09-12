34879d32aafbe7cb8eb21f6625d352d23d02c2ee
Kimi de Jeer <Kimi.deJeer@student.rocva.nl>
2024-09-12 13:27:51 +0200

    opdracht 6 grid view

--- world/views/country/index.php
+++ world/views/country/index.php
@@ -18,40 +18,35 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
 
-
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
-            'Code',
-            'Name',
-            'Continent',
-            'Region',
-          //  'SurfaceArea',
-            //'IndepYear',
-            //'Population',
-            //'LifeExpectancy',
-            //'GNP',
-            //'GNPOld',
-            //'LocalName',
-            //'GovernmentForm',
-            //'HeadOfState',
-            //'Capital',
-            //'Code2',
+        'columns' => [
+           //['class' => 'yii\grid\SerialColumn'],
             [
                 'class' => ActionColumn::className(),
+                'contentOptions' => ['style' => 'width:60px;'],
                 'urlCreator' => function ($action, Country $model, $key, $index, $column) {
                     return Url::toRoute([$action, 'Code' => $model->Code]);
-                 }
+                }
             ],
+            ['label' => 'Code', 'attribute' => 'Code', 'contentOptions' => ['style' => 'width:30px; white-space: normal;'],],
+            ['label' => 'Naam', 'attribute' => 'Name', 'contentOptions' => ['style' => 'width:300px; white-space: normal; font-weight: bold;'],],
+            ['label' => 'Hoofdstad', 'attribute' => 'Capital', 'contentOptions' => ['style' => 'width:200px; white-space: normal;'], 'format' => 'raw', 'value' => function($data) { return Html::a('Naar hoofdstad', ['/city/index', 'CitySearch[ID]' => $data->Capital]); }],
+            ['label' => 'Inwoners', 'attribute' => 'Population', 'contentOptions' => ['style' => 'width:30px; white-space: normal;'],],
+            ['label' => 'Oppervlakte', 'attribute' => 'SurfaceArea', 'contentOptions' => ['style' => 'width:30px; white-space: normal;'],'format' => 'raw','value' => function($data) { return sprintf("%8d k&#13217;", $data->SurfaceArea); }],
+            // Nieuwe kolom voor bevolkingsdichtheid
+            ['label' => 'Bevolkingsdichtheid','value' => function ($model) {return $model->SurfaceArea > 0 ? round($model->Population / $model->SurfaceArea) : 0;},'contentOptions' => ['style' => 'width:30px; white-space: normal;'],],
+
         ],
     ]); ?>
     <p>
         <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+
+
 
 </div>
